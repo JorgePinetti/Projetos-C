a506ebd8b59a895e03c0ba5b97234d2b984e4a13
Jorge Pinetti <152190124+JorgePinetti@users.noreply.github.com>
2025-08-19 18:27:41 -0300

    Aprendendo operadores relacionais

---
+++ operadoresRelacionais.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int val1, val2;
+
+    val1 = 10;
+    val2 = 12;
+
+    if (val1 > val2)
+        ;
+    printf("%d � maior que %d ? ", val1, val2);
+    printf("%d ", val1 > val2);
+}
